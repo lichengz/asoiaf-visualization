19f347977382bb9020d57902fbe24fecd4512f6f
lichengz <whitezhang@live.com>
2020-02-25 21:01:27 -0500

    episode progress slider bar

--- Assets/Scripts/TimeLineManager.cs
+++ Assets/Scripts/TimeLineManager.cs
@@ -2,33 +2,189 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
+using System;
 
 public class TimeLineManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    JSONObject j;
+    public Text expisodeCounter;
+    public Slider slider;
+    public TimeLineScritableObject timeLineAsset;
+    public List<int>[] seasons = new List<int>[8];
+    EpisodesCollection episodesCollection;
+
     void Start()
     {
-        // string encodedString = "{\"field1\": 0.5,\"field2\": \"sampletext\",\"field3\": [1,2,3]}";
-        // j = new JSONObject(encodedString);
-        // accessData(j);
-        string file = File.ReadAllText(Application.dataPath + "/Data/test.json");
-        Debug.Log(file);
-        ScenesCollection sceneCollection = JsonUtility.FromJson<ScenesCollection>(file);
-        Debug.Log(sceneCollection.scenes.Length);
+        Init();
     }
 
     // Update is called once per frame
     void Update()
     {
+        KeyInputHandler();
+    }
 
+    void OnApplicationQuit()
+    {
+        //Resources.UnloadAsset(timeLineAsset);
+    }
+
+    private void Init()
+    {
+        string file = File.ReadAllText(Application.dataPath + "/Data/episodes.json");
+        episodesCollection = JsonUtility.FromJson<EpisodesCollection>(file);
+        UpdateTimeLineAsset(episodesCollection);
+        slider.maxValue = timeLineAsset.totalNumOfScenes - 1;
+        seasons[0] = timeLineAsset.season1;
+        seasons[1] = timeLineAsset.season2;
+        seasons[2] = timeLineAsset.season3;
+        seasons[3] = timeLineAsset.season4;
+        seasons[4] = timeLineAsset.season5;
+        seasons[5] = timeLineAsset.season6;
+        seasons[6] = timeLineAsset.season7;
+        seasons[7] = timeLineAsset.season8;
+        UpdateUI();
+    }
+
+    public void UpdateUI()
+    {
+        int sceneIndex = (int)slider.value;
+        EpisodeData epiData = CalculateEpisodeData(sceneIndex);
+        timeLineAsset.currentSeason = epiData.seasonIndex;
+        timeLineAsset.currentEpisode = epiData.episodeIndex;
+        timeLineAsset.currentScene = sceneIndex;
+        expisodeCounter.text = String.Format("Season {0}, Episode {1}, Scene {2}/{3}", epiData.seasonIndex + 1, epiData.episodeIndex + 1, CalculateSceneProgrerss(sceneIndex) + 1, timeLineAsset.scenesInEachEpisode[CalculateEpiIndex(sceneIndex)]);
+    }
+
+    private int CalculateSceneProgrerss(int sceneIndex)
+    {
+        int scene = 1;
+        int tmp = 0;
+        for (int i = 0; i < timeLineAsset.scenesInEachEpisode.Count; i++)
+        {
+            tmp += timeLineAsset.scenesInEachEpisode[i];
+            if (sceneIndex < tmp)
+            {
+                return timeLineAsset.scenesInEachEpisode[i] - (tmp - sceneIndex);
+            }
+        }
+        return scene;
+    }
+
+    private int CalculateEpiIndex(int sceneIndex)
+    {
+        int epiIndex = 0;
+        int tmp = 0;
+        for (int i = 0; i < timeLineAsset.scenesInEachEpisode.Count; i++)
+        {
+            tmp += timeLineAsset.scenesInEachEpisode[i];
+            if (sceneIndex < tmp)
+            {
+                return i;
+            }
+        }
+        return epiIndex;
+    }
+
+    private EpisodeData CalculateEpisodeData(int sceneIndex)
+    {
+        EpisodeData epiData = new EpisodeData(0, 0);
+        int epiIndex = CalculateEpiIndex(sceneIndex);
+        int tmp = 0;
+        for (int i = 0; i < seasons.Length; i++)
+        {
+            tmp += seasons[i].Count;
+            if (epiIndex < tmp)
+            {
+                epiData.seasonIndex = i;
+                epiData.episodeIndex = seasons[i].Count - (tmp - epiIndex);
+                return epiData;
+            }
+        }
+        return epiData;
+    }
+
+    private void UpdateTimeLineAsset(EpisodesCollection episodesCollection)
+    {
+        if (timeLineAsset.fileLoaded) return;
+        foreach (Episode epi in episodesCollection.episodes)
+        {
+            timeLineAsset.totalNumOfScenes += epi.scenes.Length;
+            timeLineAsset.scenesInEachEpisode.Add(epi.scenes.Length);
+            if (epi.seasonNum == 1)
+            {
+                timeLineAsset.season1.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 2)
+            {
+                timeLineAsset.season2.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 3)
+            {
+                timeLineAsset.season3.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 4)
+            {
+                timeLineAsset.season4.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 5)
+            {
+                timeLineAsset.season5.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 6)
+            {
+                timeLineAsset.season6.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 7)
+            {
+                timeLineAsset.season7.Add(epi.scenes.Length);
+            }
+            if (epi.seasonNum == 8)
+            {
+                timeLineAsset.season8.Add(epi.scenes.Length);
+            }
+        }
+        timeLineAsset.fileLoaded = true;
+    }
+
+    private void KeyInputHandler()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && timeLineAsset.currentScene > 0)
+        {
+            slider.value--;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow) && timeLineAsset.currentScene < timeLineAsset.totalNumOfScenes - 1)
+        {
+            slider.value++;
+        }
     }
 
 }
 
-[System.Serializable]
-public class ScenesCollection
+public struct EpisodeData
 {
+    public int seasonIndex, episodeIndex;
+
+    public EpisodeData(int s, int e)
+    {
+        seasonIndex = s;
+        episodeIndex = e;
+    }
+}
+
+[System.Serializable]
+public class EpisodesCollection
+{
+    public Episode[] episodes;
+}
+
+[System.Serializable]
+public class Episode
+{
+    public int seasonNum;
+    public int episodeNum;
+    public string episodeTitle;
+    public string episodeDescription;
     public Scene[] scenes;
 }
 
@@ -39,5 +195,11 @@ public class Scene
     public string sceneEnd;
     public string location;
     public string subLocation;
-    public string[] characters;
+    public Character[] characters;
+}
+
+[System.Serializable]
+public class Character
+{
+    public string name;
 }
---
+++ Assets/Scripts/TimeLineScritableObject.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TimeLineScritableObject", menuName = "asoiaf/TimeLineScritableObject", order = 0)]
+public class TimeLineScritableObject : ScriptableObject
+{
+    public bool fileLoaded;
+    public int currentSeason;
+    public int currentEpisode;
+    public int currentScene;
+    public int totalNumOfScenes;
+    public List<int> scenesInEachEpisode;
+    public List<int> season1 = new List<int>();
+    public List<int> season2 = new List<int>();
+    public List<int> season3 = new List<int>();
+    public List<int> season4 = new List<int>();
+    public List<int> season5 = new List<int>();
+    public List<int> season6 = new List<int>();
+    public List<int> season7 = new List<int>();
+    public List<int> season8 = new List<int>();
+}
